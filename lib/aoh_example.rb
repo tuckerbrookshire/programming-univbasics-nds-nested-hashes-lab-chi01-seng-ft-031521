db6c7f140f46a90492e09ca6f52779fac3426a8c
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 20:25:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,7 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   occupations = [
-    {:name=>"Don Gately", :occupation=>"Live-in Staff" }
+    { name:  "Don Gately", occupation: "Live-in Staff" }
     { name:  "Joelle van Dyne", occupation: "Radio Personality" }
     { name:  "Pat Monteseian", occupation: "Staff" }
     { name:  "Kate Gompert", occupation: "None" }
