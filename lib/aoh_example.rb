cf0fe06e19d7a3f0a9d94961310d342a7d7204d2
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 20:51:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,7 +31,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[1][:name] = "Kyle Kinane"
+  aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
