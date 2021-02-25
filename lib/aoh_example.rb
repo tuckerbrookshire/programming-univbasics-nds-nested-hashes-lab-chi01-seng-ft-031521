3edd8af47503cc2eb3eee8a06b01ec22330bd4ae
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 20:48:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,6 +31,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[]
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
