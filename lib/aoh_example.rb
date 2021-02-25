4f7cba491a8bcb8f23b68cf3aad416120605098c
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 20:40:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,10 @@ end
 
 def literal_aoh
   aoh_literal = [
-    {:name=>"Don Gately", :occupation=>"Live-in Staff"}, {:name=>"Joelle van Dyne", :occupation=>"Radio ...{:name=>"Kate Gompert", :occupation=>"None"}, {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+    {:name=>"Don Gately", :occupation=>"Live-in Staff"}, 
+    {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+    {:name=>"Kate Gompert", :occupation=>"None"},
+    {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
     ]
   
   # Using Array literal syntax only, build a nested array that uses the data in
