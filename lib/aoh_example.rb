d517e0f787e9db925cb787890d7bc9f82a7b4ccf
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-25 20:30:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,7 @@ end
 
 def literal_aoh
   aoh_literal = [
-  { name:  "Don Gately", occupation: "Live-in Staff" }
+  { :name=>"Don Gately", occupation: "Live-in Staff" }
   { name:  "Joelle van Dyne", occupation: "Radio Personality" }
   { name:  "Pat Monteseian", occupation: "Staff" }
   { name:  "Kate Gompert", occupation: "None" }
